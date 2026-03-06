f407ae78ed241f8d082ce86a5096746796f88718
Victoria Pavlova <vikkkapaw@mail.ru>
2026-03-06 16:23:40 +0300

    Настройка SecurityFilterChain.

--- spring-security-example/src/main/java/ru/example/spring_security_example/config/SecurityConfig.java
+++ spring-security-example/src/main/java/ru/example/spring_security_example/config/SecurityConfig.java
@@ -2,16 +2,22 @@ package ru.example.spring_security_example.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractAuthenticationFilterConfigurer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
 
     @Bean
@@ -21,6 +27,15 @@ public class SecurityConfig {
         UserDetails vika92 = User.builder().username("vika92").password(encoder.encode("password1")).build();
 
         return new InMemoryUserDetailsManager(admin, user, vika92);
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain (HttpSecurity http) throws Exception{
+        return http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth.requestMatchers("api/v1/apps/welcome").permitAll()
+                        .requestMatchers("api/v1/apps/**").authenticated())
+                .formLogin(AbstractAuthenticationFilterConfigurer::permitAll)
+                .build();
     }
 
     @Bean
